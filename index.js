48f5b4a1bcb6763e1a716e52b2732d01d63521cc
Subhamtyagi684 <subhamtyagi85@gmail.com>
2021-11-07 19:42:33 +0530

    modified camera setting

--- index.js
+++ index.js
@@ -10029,10 +10029,9 @@ var __Html5QrcodeLibrary__;
                     return "Scan an Image File"
                 }, t.textIfFileScanSelected = function() {
                     return "Scan using camera directly"
-                // }, t.selectCamera = function() {
-                //     return "Select Camera"
-                // },
-                ,t
+                }, t.selectCamera = function() {
+                    return "Select Camera"
+                }, t
             }(),
             f = function() {
                 function t() {}
@@ -11037,10 +11036,8 @@ var __Html5QrcodeLibrary__;
                     o = document.createElement("select");
                 if (1 === i) o.style.display = "none";
                 else {
-                    // var s = I.selectCamera();
-                    // n.innerText = s + " (" + t.length + ")  "
-                    var s ='';
-                    n.innerText = ""
+                    var s = I.selectCamera();
+                    n.innerText = s + " (" + t.length + ")  "
                 }
                 o.id = this.getCameraSelectionId();
                 for (var a = [], A = 0, l = t; A < l.length; A++) {
@@ -11052,7 +11049,7 @@ var __Html5QrcodeLibrary__;
                     S.innerText = g, a.push(S),
                     o.appendChild(S)
                     }
-
+                    
                 }
                 n.appendChild(o), r.appendChild(n);
                 var d = document.createElement("span"),
